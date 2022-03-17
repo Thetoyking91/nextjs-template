5b51918187be681de634023a9f9316aeb14b6190
Ryan <ryanphilipmccoy@gmail.com>
2022-03-17 14:23:34 -0700

    general updates

--- public/navbar.js
+++ public/navbar.js
@@ -4,7 +4,7 @@ import React from "react"
 export class Navbar extends React.Component {
     render() {
         return <nav className="topnav">
-            <Link href="/"><a className="text-xl text-blue-700">Niblet</a></Link>
+            <Link href="/"><a className="text-xl text-blue-600">Niblet</a></Link>
         </nav>
     }
 }
