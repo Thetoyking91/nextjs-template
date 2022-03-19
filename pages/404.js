4a4c2d1adf77fe89709f5a1351dc4360884028bd
Ryan <ryanphilipmccoy@gmail.com>
2022-03-18 20:45:14 -0700

    fix 404

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,6 @@ import Template from "../public/Template";
 
 export default function error() {
     return (
-        <Template title="404" headText="404: Page not found."
+        <Template title="404" headText="404: Page not found." />
     )
 }
